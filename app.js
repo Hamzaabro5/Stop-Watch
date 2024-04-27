5e81f3a7ddade3df7c4fc42cdbd2874d8b9147bb
Hamzaabro5 <hamzaabro683@gmail.com>
2024-04-27 17:23:33 +0500

    Stopwatch

--- app.js
+++ app.js
@@ -1,40 +1,80 @@
 const hour = document.querySelector('#hour');
 const minutes = document.querySelector('#min');
 const seconds = document.querySelector('#sec');
+const mili = document.querySelector('#milisec');
 
 
+let milisec = 0;
 let sec = 0;
 let min = 0;
 let houur = 0;
 let startInterval ;
 
+// function startWatch (){
+//     clearInterval(startInterval)
+//     startInterval =  setInterval(function(){
+//         sec += 1
+//         seconds.innerHTML = sec
+
+//         if (sec === 60) {
+//             sec = 0
+//             seconds.innerHTML = 0
+            
+//             min += 1
+//             minutes.innerHTML = min
+
+//             if (min === 60) {
+//                 min = 0
+//                 minutes.innerHTML = min
+
+//                 houur += 1
+//                 hour.innerHTML = houur
+//             }
+            
+//         }
+//     } , 1000);
+
+    
+
+// }
+
 function startWatch (){
     clearInterval(startInterval)
     startInterval =  setInterval(function(){
-        sec += 1
-        seconds.innerHTML = sec
+        milisec += 1
+        mili.innerHTML = milisec
 
-        if (sec === 60) {
-            sec = 0
-            seconds.innerHTML = 0
+        if (milisec === 100) {
+            milisec = 0
+            mili.innerHTML = 0
             
-            min += 1
-            minutes.innerHTML = min
+            sec += 1
+            seconds.innerHTML = sec
 
+            if (sec === 60) {
+                sec = 0
+                seconds.innerHTML = sec
+
+                min += 1
+                minutes.innerHTML = min
+
+            }
             if (min === 60) {
                 min = 0
-                minutes.innerHTML = min
+                min.innerHTML = min
 
                 houur += 1
                 hour.innerHTML = houur
             }
             
         }
-    } , 1000);
+    } , 10);
 
     
 
 }
+
+
 
 function stopInterval(){
     clearInterval(startInterval);
@@ -42,11 +82,18 @@ function stopInterval(){
 
 
 function resetWatch() {
+    milisec = 0
     sec = 0
     min = 0
     houur = 0
+    mili.innerHTML = 0
     seconds.innerHTML = 0
     minutes.innerHTML = 0
     hour.innerHTML = 0
     clearInterval(startInterval);
 }
+
+
+
+
+
