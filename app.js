e37f750018c53113848e37f59688b721ef960ae8
Hamzaabro5 <hamzaabro683@gmail.com>
2024-06-24 10:01:56 +0500

    StopWatch

--- app.js
+++ app.js
@@ -10,33 +10,6 @@ let min = 0;
 let houur = 0;
 let startInterval ;
 
-// function startWatch (){
-//     clearInterval(startInterval)
-//     startInterval =  setInterval(function(){
-//         sec += 1
-//         seconds.innerHTML = sec
-
-//         if (sec === 60) {
-//             sec = 0
-//             seconds.innerHTML = 0
-            
-//             min += 1
-//             minutes.innerHTML = min
-
-//             if (min === 60) {
-//                 min = 0
-//                 minutes.innerHTML = min
-
-//                 houur += 1
-//                 hour.innerHTML = houur
-//             }
-            
-//         }
-//     } , 1000);
-
-    
-
-// }
 
 function startWatch (){
     clearInterval(startInterval)
